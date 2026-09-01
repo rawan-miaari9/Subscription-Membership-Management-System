d484e971d797a40a559fc6d873fa99b3183a0afa
kassemamin22-creator <kassem.amin.22@gmail.com>
2026-09-01 18:34:47 +0300

    Create MembershipPlan model (SMM2-148)

--- managementSystem/core/models.py
+++ managementSystem/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-# Frontend-only mode - no backend models yet
-# Models will be added later when backend is enabled
+
+
+class MembershipPlan(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, null=True, blank=True)
+    duration_days = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_fixed = models.BooleanField(null=True, blank=True, default=True)
+    is_active = models.BooleanField(null=True, blank=True, default=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'membership_plans'
+
+    def __str__(self):
+        return self.name
